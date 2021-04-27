a65f6851b844e802d6c20015513b19a179cea8d3
aaronfeingold <ajfeingold88@gmail.com>
2021-04-27 18:53:54 -0500

    folder move

---
+++ src/components/TimerView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleTimer } from '../actions';
+import './TimerView.css'
+
+export default function TimerView(props) {
+  const { index, timer } = props
+  const dispatch = useDispatch()
+
+  return(
+    <div className="TimerView">
+      <h1>{timer.name}</h1>
+      <h2>{timer.time}</h2>
+      <button onClick={() => dispatch(toggleTimer(index))}> {timer.isRunning ? "Stop" : "Start"}</button>
+    </div>
+  )
+}
