9cbf52d59883e1e4b060a2f9e3bf7fb2e12da0cb
aaronfeingold <ajfeingold88@gmail.com>
2021-04-27 21:13:21 -0500

    persisting timers

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,31 @@
+const YOGA_STATE = "YOGA_STATE"
+
+export const loadState = () => {
+  try{
+    const serializedState = localStorage.getItem(YOGA_STATE)
+    if (serializedState === null){
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch(err) {
+    return undefined
+  }
+};
+
+export const saveState = (state) => {
+  try{
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem(YOGA_STATE, serializedState)
+  } catch(err) {
+    console.log("Error saving data")
+  }
+}
+
+
+
+
+
+
 export const formatTime = (time) => {
   const ms = Math.round(time / 100) % 10
   const secs = Math.floor(time / 1000) % 60
